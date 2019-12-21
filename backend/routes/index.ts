0381a1bd13dd36b64de76853e6a0f30629d569b5
노정훈 <fireworks2255@gmail.com>
2019-12-22 05:31:30 +0900

    Connect controller to routes

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import * as ApiController from '../controllers/api';
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(_, res) {
-  res.json('hi');
-});
+router.get('/theater/code/:code', ApiController.getTheaterInfo);
+router.get('/movie/code/:code', ApiController.getMovieInfoByCode);
+router.get('/movie/name/:name', ApiController.getMovieInfoByName);
+router.get('/theater/near', ApiController.nearestTheaters);
+router.get('/movies-near', ApiController.nearestMovies);
 
 export default router;
